b2cd5143d2b0c2473fd66724bd498ebe5e25dfca
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-16 21:04:07 +0530

    add scripts generalCars.js file and link to generalCars.html file in SpringFrontEnd

---
+++ Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/generalCars.js
@@ -0,0 +1,145 @@
+$("#all").css('display','block');
+$("#NoOfPassengers").css('display','none');
+$("#four").css('display','none');
+$("#transmissionType").css('display','none');
+$("#manual").css('display','none');
+$("#auto").css('display','none');
+$("#brand").css('display','none');
+$("#suzuki").css('display','none');
+$("#toyota").css('display','none');
+$("#perodua").css('display','none');
+$("#price").css('display','none');
+$("#fuel").css('display','none');
+$("#petrol").css('display','none');
+$("#diesel").css('display','none');
+$("#rowNoOfPassengers").css('display','none');
+$("#rowTransmissionType").css('display','none');
+$("#rowBrand").css('display','none');
+$("#rowFuelType").css('display','none');
+
+
+$("#navAll").click(function (){
+    $("#all").css('display','block');
+    $("#NoOfPassengers").css('display','none');
+    $("#four").css('display','none');
+    $("#transmissionType").css('display','none');
+    $("#manual").css('display','none');
+    $("#auto").css('display','none');
+    $("#brand").css('display','none');
+    $("#suzuki").css('display','none');
+    $("#toyota").css('display','none');
+    $("#perodua").css('display','none');
+    $("#price").css('display','none');
+    $("#fuel").css('display','none');
+    $("#petrol").css('display','none');
+    $("#diesel").css('display','none');
+    $("#rowNoOfPassengers").css('display','none');
+    $("#rowTransmissionType").css('display','none');
+    $("#rowBrand").css('display','none');
+    $("#rowFuelType").css('display','none');
+});
+
+$("#navNoOfPassengers").click(function (){
+    $("#all").css('display','none');
+    $("#NoOfPassengers").css('display','block');
+    $("#four").css('display','block');
+    $("#transmissionType").css('display','none');
+    $("#manual").css('display','none');
+    $("#auto").css('display','none');
+    $("#brand").css('display','none');
+    $("#suzuki").css('display','none');
+    $("#toyota").css('display','none');
+    $("#perodua").css('display','none');
+    $("#price").css('display','none');
+    $("#fuel").css('display','none');
+    $("#petrol").css('display','none');
+    $("#diesel").css('display','none');
+    $("#rowNoOfPassengers").css('display','block');
+    $("#rowTransmissionType").css('display','none');
+    $("#rowBrand").css('display','none');
+    $("#rowFuelType").css('display','none');
+});
+
+$("#navTransmissionType").click(function (){
+    $("#all").css('display','none');
+    $("#NoOfPassengers").css('display','none');
+    $("#four").css('display','none');
+    $("#transmissionType").css('display','block');
+    $("#manual").css('display','block');
+    $("#auto").css('display','block');
+    $("#brand").css('display','none');
+    $("#suzuki").css('display','none');
+    $("#toyota").css('display','none');
+    $("#perodua").css('display','none');
+    $("#price").css('display','none');
+    $("#fuel").css('display','none');
+    $("#petrol").css('display','none');
+    $("#diesel").css('display','none');
+    $("#rowNoOfPassengers").css('display','none');
+    $("#rowTransmissionType").css('display','block');
+    $("#rowBrand").css('display','none');
+    $("#rowFuelType").css('display','none');
+});
+
+$("#navBrand").click(function (){
+    $("#all").css('display','none');
+    $("#NoOfPassengers").css('display','none');
+    $("#four").css('display','none');
+    $("#transmissionType").css('display','none');
+    $("#manual").css('display','none');
+    $("#auto").css('display','none');
+    $("#brand").css('display','block');
+    $("#suzuki").css('display','block');
+    $("#toyota").css('display','block');
+    $("#perodua").css('display','block');
+    $("#price").css('display','none');
+    $("#fuel").css('display','none');
+    $("#petrol").css('display','none');
+    $("#diesel").css('display','none');
+    $("#rowNoOfPassengers").css('display','none');
+    $("#rowTransmissionType").css('display','none');
+    $("#rowBrand").css('display','block');
+    $("#rowFuelType").css('display','none');
+});
+
+$("#navPrice").click(function (){
+    $("#all").css('display','none');
+    $("#NoOfPassengers").css('display','none');
+    $("#four").css('display','none');
+    $("#transmissionType").css('display','none');
+    $("#manual").css('display','none');
+    $("#auto").css('display','none');
+    $("#brand").css('display','none');
+    $("#suzuki").css('display','none');
+    $("#toyota").css('display','none');
+    $("#perodua").css('display','none');
+    $("#price").css('display','block');
+    $("#fuel").css('display','none');
+    $("#petrol").css('display','none');
+    $("#diesel").css('display','none');
+    $("#rowNoOfPassengers").css('display','none');
+    $("#rowTransmissionType").css('display','none');
+    $("#rowBrand").css('display','none');
+    $("#rowFuelType").css('display','none');
+});
+
+$("#navFuel").click(function (){
+    $("#all").css('display','none');
+    $("#NoOfPassengers").css('display','none');
+    $("#four").css('display','none');
+    $("#transmissionType").css('display','none');
+    $("#manual").css('display','none');
+    $("#auto").css('display','none');
+    $("#brand").css('display','none');
+    $("#suzuki").css('display','none');
+    $("#toyota").css('display','none');
+    $("#perodua").css('display','none');
+    $("#price").css('display','none');
+    $("#fuel").css('display','block');
+    $("#petrol").css('display','block');
+    $("#diesel").css('display','block');
+    $("#rowNoOfPassengers").css('display','none');
+    $("#rowTransmissionType").css('display','none');
+    $("#rowBrand").css('display','none');
+    $("#rowFuelType").css('display','block');
+});
