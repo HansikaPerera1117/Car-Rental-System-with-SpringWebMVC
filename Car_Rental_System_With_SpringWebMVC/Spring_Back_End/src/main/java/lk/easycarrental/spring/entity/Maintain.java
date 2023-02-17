b247ea90ca4615c2c7af182ed1c11b965a6c4974
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 17:30:10 +0530

    add properties to Maintain entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Maintain.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Maintain.java
@@ -1,4 +1,25 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Maintain {
+    @Id
+    public String MID;
+    public String description;
+    public String status;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH,CascadeType.DETACH})
+    @JoinColumn(name = "registrationNumber",referencedColumnName = "registrationNumber",nullable = false)
+    private Car registrationNumber;
+
 }
