727b14aebfe44421d5fd2bbe757815ba92893ad1
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 17:21:08 +0530

    add properties to Driver entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Driver.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Driver.java
@@ -1,4 +1,26 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Driver {
+    @Id
+    public String driverID;
+    public String name;
+    public String address;
+    public String contactNo;
+    public String NIC;
+    public String drivingLicense;
+    public String availability;
+
 }
