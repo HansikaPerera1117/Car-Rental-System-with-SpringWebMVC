2c91697d8303e979bfa24cfec0c936297dc76d67
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 16:51:24 +0530

    add properties to Admin entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Admin.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Admin.java
@@ -1,4 +1,25 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Admin{
+
+    @Id
+    private String adminID;
+    private String name;
+    private String contactNo;
+    private String username;
+    private String password;
+
 }
