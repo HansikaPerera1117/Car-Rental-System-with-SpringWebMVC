20197867d0fd9cd098c0e0413f4aec66830f6bf3
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 17:15:32 +0530

    add properties to Car entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Car.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Car.java
@@ -1,4 +1,35 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Car {
+    @Id
+    public String registrationNumber;
+    public String brand;
+    public String type;
+    public String frontImage;
+    public String backImage;
+    public String sideImage;
+    public String interiorImage;
+    public String noOfPassengers;
+    public String transmissionType;
+    public String fuelType;
+    public String dailyRate;
+    public String monthlyRate;
+    public String freeKMForADay;
+    public String freeKMForAMonth;
+    public String pricePerExtraKM;
+    public String color;
+    public String availability;
 }
