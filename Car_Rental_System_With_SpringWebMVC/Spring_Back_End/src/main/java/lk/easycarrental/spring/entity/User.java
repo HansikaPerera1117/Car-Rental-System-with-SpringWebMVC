d840741d03256f94c7431ba8eaed3b1cfb1316d4
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 17:18:27 +0530

    add properties to User entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/User.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/User.java
@@ -1,4 +1,29 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class User {
+    @Id
+    public String userID;
+    public String name;
+    public String address;
+    public String contactNo;
+    public String email;
+    public String NIC;
+    public String imageOfNIC;
+    public String drivingLicense;
+    public String imageOfDrivingLicense;
+    public String username;
+    public String password;
 }
