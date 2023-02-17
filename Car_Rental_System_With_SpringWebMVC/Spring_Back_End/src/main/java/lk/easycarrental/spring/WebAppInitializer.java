eb652e50974f8093c7f54aaabf105dff2f94245a
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 15:40:39 +0530

    add configurations to WebAppInitializer file in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/WebAppInitializer.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/WebAppInitializer.java
@@ -1,4 +1,22 @@
 package lk.easycarrental.spring;
 
-public class WebAppInitializer {
+import lk.easycarrental.spring.config.WebAppConfig;
+import lk.easycarrental.spring.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
