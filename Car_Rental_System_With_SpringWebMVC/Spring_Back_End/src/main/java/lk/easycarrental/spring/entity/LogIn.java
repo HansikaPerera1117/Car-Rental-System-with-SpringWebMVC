a5e024fd94a87b5aafda3801c9e734ddecb52df0
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-17 16:54:25 +0530

    add properties to LogIn entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/LogIn.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/LogIn.java
@@ -1,5 +1,23 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class LogIn {
 
+    @Id
+    private String loginID;
+    private String role;
+    private String username;
+    private String password;
 }
