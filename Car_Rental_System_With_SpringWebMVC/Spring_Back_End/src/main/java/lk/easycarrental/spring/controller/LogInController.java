a7b221fb64e40e195aa5a91b5fbe3456e509b9b9
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-18 16:49:49 +0530

    create  LogInController class in controller package in SpringBackEnd

---
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/controller/LogInController.java
@@ -0,0 +1,10 @@
+package lk.easycarrental.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LogInController {
+
+}
