251de2b52b025919b29f6301966feaddcd1f4a86
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-18 16:56:28 +0530

    add classes to @ComponentScan annotation in WebAppConfig class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/WebAppConfig.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/WebAppConfig.java
@@ -1,11 +1,13 @@
 package lk.easycarrental.spring.config;
 
+import lk.easycarrental.spring.advice.AppWideExceptionHandler;
+import lk.easycarrental.spring.controller.LogInController;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackageClasses = {})
+@ComponentScan(basePackageClasses = {LogInController.class, AppWideExceptionHandler.class})
 public class WebAppConfig {
 }
