969bac83cbe8e1948426864cf7adcd1dc5462956
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-18 16:51:48 +0530

    create  AppWideExeptionHandler class in advice package in SpringBackEnd

---
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/advice/AppWideExceptionHandler.java
@@ -0,0 +1,9 @@
+package lk.easycarrental.spring.advice;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+}
--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/controller/LogInController.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/controller/LogInController.java
@@ -1,10 +1,12 @@
 package lk.easycarrental.spring.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin
 public class LogInController {
 
 }
