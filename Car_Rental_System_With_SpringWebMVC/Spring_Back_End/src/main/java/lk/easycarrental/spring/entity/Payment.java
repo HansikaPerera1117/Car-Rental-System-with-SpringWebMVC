73f98da91d7579a41272d3712f9038ab16880154
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-18 16:42:57 +0530

    add properties to Payment entity class in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Payment.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/entity/Payment.java
@@ -1,4 +1,30 @@
 package lk.easycarrental.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+@Entity
 public class Payment {
+    @Id
+    public String paymentID;
+    public String date;
+    public String damageCharge;
+    public String returnLossDamageWaiver;
+    public String rentPrice;
+    public String extraKM;
+    public String priseForExtraKM;
+    public String driverPayment;
+    public String totalPayment;
+
+    @OneToOne
+    @JoinColumn(name = "rentID")
+    private Rent rentID;
 }
