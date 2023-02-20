a7d2296ab0df8b6a21d3b5358e3e28873ae47ddf
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-20 11:37:45 +0530

    add script to menu bar when click menu item and connect strat page when ligoutin adminDashboard.html html file in SpringFrontEnd

---
+++ Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/adminDashboard.js
@@ -0,0 +1,44 @@
+$("#menuHome").click(function (){
+    $("#menuCars, #menuUsers, #menuDrivers, #menuRents, #menuPayments, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuHome").css("background-color", "#B7B7B7");
+});
+
+$("#menuCars").click(function (){
+    $("#menuHome, #menuUsers, #menuDrivers, #menuRents, #menuPayments, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuCars").css("background-color", "#B7B7B7");
+});
+
+$("#menuUsers").click(function (){
+    $("#menuHome, #menuCars, #menuDrivers, #menuRents, #menuPayments, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuUsers").css("background-color", "#B7B7B7");
+});
+
+$("#menuDrivers").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuRents, #menuPayments, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuDrivers").css("background-color", "#B7B7B7");
+});
+
+$("#menuRents").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuDrivers, #menuPayments, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuRents").css("background-color", "#B7B7B7");
+});
+
+$("#menuPayments").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuDrivers, #menuRents, #menuRequests, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuPayments").css("background-color", "#B7B7B7");
+});
+
+$("#menuRequests").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuDrivers, #menuRents, #menuPayments, #menuIncomes, #menuMaintains").css("background-color", "white");
+    $("#menuRequests").css("background-color", "#B7B7B7");
+});
+
+$("#menuIncomes").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuDrivers, #menuRents, #menuPayments, #menuRequests, #menuMaintains").css("background-color", "white");
+    $("#menuIncomes").css("background-color", "#B7B7B7");
+});
+
+$("#menuMaintains").click(function (){
+    $("#menuHome, #menuCars, #menuUsers, #menuDrivers, #menuRents, #menuPayments, #menuRequests, #menuIncomes").css("background-color", "white");
+    $("#menuMaintains").css("background-color", "#B7B7B7");
+});
