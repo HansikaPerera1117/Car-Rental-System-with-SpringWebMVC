e58ebd7fc91d3006ae89cb95de1f28fae5106fa5
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-21 19:31:05 +0530

    add configurations ro JPAConfig class in config directory in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/JPAConfig.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/JPAConfig.java
@@ -1,11 +1,24 @@
 package lk.easycarrental.spring.config;
 
+import lk.easycarrental.spring.repo.UserRepo;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackageClasses = {})
+@EnableJpaRepositories(basePackageClasses = {UserRepo.class})
 public class JPAConfig {
+
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setPackagesToScan("lk.easycarrental.spring.entity");
+        bean.setDataSource(ds);
+        bean.setJpaVendorAdapter(jpa);
+        return bean;
+    }
 }
