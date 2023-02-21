5ba0d2340c68cef5992646ea1afe73a857aae64a
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-21 19:34:41 +0530

    add configurations ro JPAConfig class in config directory in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/JPAConfig.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/config/JPAConfig.java
@@ -1,8 +1,10 @@
 package lk.easycarrental.spring.config;
 
 import lk.easycarrental.spring.repo.UserRepo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,11 +16,22 @@ import javax.sql.DataSource;
 @EnableJpaRepositories(basePackageClasses = {UserRepo.class})
 public class JPAConfig {
 
+    @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter jpa){
         LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
         bean.setPackagesToScan("lk.easycarrental.spring.entity");
         bean.setDataSource(ds);
         bean.setJpaVendorAdapter(jpa);
         return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource ds =new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/CarRentalSystem?createIfNotExist=true");
+        ds.setUsername("root");
+        ds.setPassword("1234");
+        return ds;
     }
 }
