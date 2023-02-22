ee9137d7b60ed2d71d4c8df259d7b7e7d9358c27
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-22 16:48:57 +0530

    implement login logic LoginController.js in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/LogInController.js
+++ Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/LogInController.js
@@ -16,3 +16,28 @@ function getNewLoginId() {
         }
     });
 }
+
+$('#btnLogIn').click(function () {
+    var role = $('#inputRole').find('option:selected').text();
+
+    if ($('#inputUsername').val() != "" && $('#inputPassword').val() != "" && role != "Select Your Role") {
+        login();
+    }
+});
+
+function login() {
+    var username = $('#inputUsername').val();
+    var password = $('#inputPassword').val();
+    var role = $('#inputRole').find('option:selected').text();
+
+    console.log(role);
+
+    if (role === "Admin") {
+        searchAdmin(role, username, password);
+    } else if (role === "User") {
+        searchCustomer(role, username, password);
+    } else if (role === "Driver") {
+        searchDriver(role, username, password);
+    }
+}
+
