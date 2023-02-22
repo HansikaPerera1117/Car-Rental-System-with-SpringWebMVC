464e2c393e50154bd87c12f7907c434e2533e6e9
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-22 21:47:51 +0530

    implement to add user details to user profile input fields in UserDashboardController in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/UserDashboardController.js
+++ Car_Rental_System_With_SpringWebMVC/Spring_Front_End/controllers/UserDashboardController.js
@@ -9,11 +9,44 @@ getLastLoginUser();
 
 function getLastLoginUser() {
     $.ajax({
-        url: "login/getLastLogin",
+        url: "http://localhost:8080/Spring_Back_End_war/login/getLastLogin",
         method: "GET",
         success: function (res) {
-            let login = res.data;
-            console.log(login.logInId);
+            let loginData = res.data;
+            console.log(loginData);
+            getAllUserData(loginData.username, loginData.password);
         }
     })
+}
+
+function getAllUserData(username, password) {
+    $.ajax({
+        url: "http://localhost:8080/Spring_Back_End_war/user/set/" + username + "/" + password,
+        method: "GET",
+        success: function (res) {
+            let user = res.data;
+            setCustomerDetails(user);
+            //loadMyCarRentsToTable(user.userID);
+        }
+    })
+}
+
+function setCustomerDetails(user) {
+    console.log(user);
+    $('#UserID').text(user.userID);
+    $('#UserName').text(user.name);
+    $('#UserProfileID').text(user.userID);
+
+    $('#inputName').val(user.name);
+    $('#inputAddress').val(user.address);
+    $('#inputContact').val(user.contactNo);
+    $('#inputEmail').val(user.email);
+    $('#inputNIC').val(user.nic);
+    $('#inputDrivingLicense').val(user.drivingLicense);
+    $('#inputUsername').val(user.username);
+    $('#inputPassword').val(user.password);
+
+    // upadte eka image wenama anith details wenama karanna
+
+
 }
