c9b767ad166cfa87e24568a104e85b7d2f929e6f
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-02-24 13:44:12 +0530

    create addrent,update,delete,search,getall,generateRentId methods in rentController,rentServiceImpl classes,rentService,driverRepo interfaces in SpringBackEnd

--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/service/RentService.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/service/RentService.java
@@ -1,4 +1,21 @@
 package lk.easycarrental.spring.service;
 
+import lk.easycarrental.spring.dto.RentDTO;
+
+import java.util.List;
+
 public interface RentService {
+
+    void addRent(RentDTO dto);
+
+    void updateRent(RentDTO dto);
+
+    void deleteRent(String rentId);
+
+    RentDTO searchRent(String rentId);
+
+    List<RentDTO> getAllRents();
+
+    String generateRentId();
+
 }
--- Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/service/impl/RentServiceImpl.java
+++ Car_Rental_System_With_SpringWebMVC/Spring_Back_End/src/main/java/lk/easycarrental/spring/service/impl/RentServiceImpl.java
@@ -1,12 +1,43 @@
 package lk.easycarrental.spring.service.impl;
 
 
+import lk.easycarrental.spring.dto.RentDTO;
 import lk.easycarrental.spring.service.RentService;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class RentServiceImpl implements RentService {
+    @Override
+    public void addRent(RentDTO dto) {
+        
+    }
+
+    @Override
+    public void updateRent(RentDTO dto) {
+
+    }
+
+    @Override
+    public void deleteRent(String rentId) {
+
+    }
+
+    @Override
+    public RentDTO searchRent(String rentId) {
+        return null;
+    }
+
+    @Override
+    public List<RentDTO> getAllRents() {
+        return null;
+    }
+
+    @Override
+    public String generateRentId() {
+        return null;
+    }
 }
